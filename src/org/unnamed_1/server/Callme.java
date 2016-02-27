450e906d9be522ca6cb0c24b1a5835c512b5f1c3
lory <lorenzo23minecraft@hotmail.it>
2016-02-27 19:21:06 +0100

    changes

--- src/org/unnamed_1/server/Callme.java
+++ src/org/unnamed_1/server/Callme.java
@@ -3,12 +3,18 @@ package org.unnamed_1.server;
 public class Callme {
 
     private static Server server;
+    private static InputManager inputManager;
 
     public static void main(String[] args) {
         server = new Server(9090);
+        inputManager = new InputManager();
     }
 
     public static Server getServer() {
         return server;
+    }
+
+    public static InputManager getInputManager() {
+        return inputManager;
     }
 }
