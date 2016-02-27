bbce2b4f26e334f4181fae067032bafec76a5855
lory <lorenzo23minecraft@hotmail.it>
2016-02-27 19:22:47 +0100

    changes

--- src/org/unnamed_1/server/InputManager.java
+++ src/org/unnamed_1/server/InputManager.java
@@ -1,9 +1,20 @@
 package org.unnamed_1.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class InputManager extends Thread {
+
+    private final BufferedReader reader;
+
+    public InputManager() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+    }
 
     @Override
     public void run() {
+        while (true) {
 
+        }
     }
 }
